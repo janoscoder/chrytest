3c210fc821c3ecba48012b30cde0d384cb991180
janoscoder <janos.boudet@gmail.com>
2018-11-20 22:27:34 +0100

    rajout du code du premier bouton
    pour le moment, on fait apparaitre un toast avevc le texte t'as clic

--- TestApp1/app/src/main/java/com/example/janos/testapp1/MainActivity.kt
+++ TestApp1/app/src/main/java/com/example/janos/testapp1/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.janos.testapp1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onBtnActChoice(view: View){
+        val myToast = Toast.makeText(this,"T'as clic!", Toast.LENGTH_SHORT)
+        myToast.show()
     }
 }
