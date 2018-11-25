24d0a2c6d4d44b279d84b7b4044043d136a68092
janoscoder <janos.boudet@gmail.com>
2018-11-25 16:03:06 +0100

    trying buttons

--- TestApp1/app/src/main/java/com/example/janos/testapp1/ChoiceActivity.kt
+++ TestApp1/app/src/main/java/com/example/janos/testapp1/ChoiceActivity.kt
@@ -61,7 +61,8 @@ class ChoiceActivity : AppCompatActivity() {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        dummy_button.setOnTouchListener(mDelayHideTouchListener)
+        //dummy_button.setOnTouchListener(mDelayHideTouchListener)
+        button_left.offsetLeftAndRight(100)
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
